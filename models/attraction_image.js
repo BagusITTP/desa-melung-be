78af5f7215a95917392b13da36a4e80e4b3fdc23
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-12-23 11:10:45 +0700

    feat(attraction_image): add model and migration

---
+++ models/attraction_image.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class attraction_image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  attraction_image.init({
+    name: DataTypes.STRING,
+    fileId: DataTypes.STRING,
+    url: DataTypes.STRING,
+    attraction_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'attraction_image',
+  });
+  return attraction_image;
+};
