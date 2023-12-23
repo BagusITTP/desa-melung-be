ba9bdabda246be5d723e844e709c0163e2308d20
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-12-23 11:09:59 +0700

    feat(facilities_tour): add model and migration

---
+++ models/facilities_tour.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class facilities_tour extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  facilities_tour.init({
+    name: DataTypes.STRING,
+    tour_package_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'facilities_tour',
+  });
+  return facilities_tour;
+};
