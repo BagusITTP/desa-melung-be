982261be363a5f1b32febacd9c123e4e8a805e44
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-12-23 11:09:42 +0700

    feat(tour_image): add model and migration

---
+++ models/tour_image.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class tour_image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  tour_image.init({
+    name: DataTypes.STRING,
+    fileId: DataTypes.STRING,
+    url: DataTypes.STRING,
+    tour_package_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'tour_image',
+  });
+  return tour_image;
+};
