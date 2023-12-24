7c6f5d99ba7b05597b75dd69ad7a8b11a37f99d3
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-12-24 16:21:36 +0700

    fix(facilities_attraction): delete all

--- routes/facilitiesAttraction.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const auth = require('../middleware/auth')
-const facilitiesAttractionController = require('../controllers/facilitiesAttractionController')
-const checkRole = require('../middleware/checkRole')
-
-router.get("/", facilitiesAttractionController.getFacilitiesAttraction)
-router.delete("/:id", auth, checkRole(["admin"]), facilitiesAttractionController.deleteFacilitiesAttraction)
-
-module.exports = router
