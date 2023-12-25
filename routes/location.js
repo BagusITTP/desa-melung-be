0e266bc84b3f71e8edb31649c553622f4c105f6f
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-12-25 13:30:24 +0700

    fix(location): delete all

--- routes/location.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const auth = require('../middleware/auth')
-const locationController = require('../controllers/locationController')
-const checkRole = require('../middleware/checkRole')
-
-router.get("/", locationController.getLocation)
-router.delete("/:id", auth, checkRole(["admin"]), locationController.deleteLocation)
-
-module.exports = router
