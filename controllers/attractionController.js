10689679650febcf2c74b78250f724e76d63f464
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2023-12-29 21:13:25 +0700

    perf(attraction): update code and reponse API

--- controllers/attractionController.js
+++ controllers/attractionController.js
@@ -35,16 +35,14 @@ const getAttraction = async (req, res) => {
 }
 
 const updateAttraction = async (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
+  const dataId = await attraction.findOne({ where: { id } });
 
-  const dataId = await attraction.findOne({ where: { id } })
-
-  // TODO: Validasi apakah id ada
   if (dataId === null) {
     return res.status(404).json({
       status: 'failed',
       message: `Data tidak ditemukan`
-    })
+    });
   }
 
   const schema = Joi.object({
@@ -54,75 +52,65 @@ const updateAttraction = async (req, res) => {
     description: Joi.string().required().label("Deskripsi"),
     facilities: Joi.array().items(Joi.string()).required().label("Fasilitas"),
     locations: Joi.array().items(Joi.string()).required().label("Lokasi"),
-  })
+  });
 
-  const val = schema.validate(req.body)
+  const val = schema.validate(req.body);
 
-  if (!(val.error)) {
-    try {
-      const { motor_price, mobil_price, ticket_price, description, facilities, locations } = val.value
-
-      if (req.files) {
-        for (let i = 0; i < req.files.length; i++) {
-          const photo = req.files[i];
-          // get extension file
-          const split = photo.originalname.split('.');
-          const ext = split[split.length - 1];
-
-          // proses upload file ke imagekit
-          const img = await ImageKit.upload({
-            file: photo.buffer, // required
-            fileName: `IMG-${Date.now()}.${ext}`
-          })
-
-          await attraction_image.create({
-            name: photo.originalname,
-            fileId: img.fileId,
-            url: img.url,
-            attraction_id: id
-          })
-        }
-      }
-
-      await attraction.update({
-        ticket_price,
-        description,
-        facilities,
-        locations
-      }, {
-        where: {
-          id
-        }
-      })
-
-      for (let i = 1; i < 3; i++) {
-        await vehicle.update({
-          price: i === 1 ? motor_price : mobil_price
-        }, {
-          where: {
-            id: i
-          }
-        })
-      }
-
-      return res.status(200).json({
-        status: 'success',
-        message: `Data telah berhasil terubah`,
-      })
-    } catch (error) {
-      return res.status(400).json({
-        status: "failed",
-        message: error.message
-      })
-    }
-  } else {
-    const message = val.error.details[0].message
+  if (val.error) {
+    const message = val.error.details[0].message;
     return res.status(400).json({
       status: "failed",
       message
-    })
+    });
   }
-}
+
+  try {
+    const { motor_price, mobil_price, ticket_price, description, facilities, locations } = val.value;
+
+    if (req.files) {
+      for (const photo of req.files) {
+        const split = photo.originalname.split('.');
+        const ext = split[split.length - 1];
+
+        const img = await ImageKit.upload({
+          file: photo.buffer,
+          fileName: `IMG-${Date.now()}.${ext}`
+        });
+
+        await attraction_image.create({
+          name: photo.originalname,
+          fileId: img.fileId,
+          url: img.url,
+          attraction_id: id
+        });
+      }
+    }
+
+    await attraction.update({
+      ticket_price,
+      description,
+      facilities,
+      locations
+    }, {
+      where: { id }
+    });
+
+    await Promise.all([
+      vehicle.update({ price: motor_price }, { where: { id: 1 } }),
+      vehicle.update({ price: mobil_price }, { where: { id: 2 } })
+    ]);
+
+    return res.status(200).json({
+      status: 'success',
+      message: `Data telah berhasil terubah`,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: "failed",
+      message: error.message
+    });
+  }
+};
 
 const deleteAttraction = async (req, res) => {
   try {
