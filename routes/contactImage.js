27b927301fc1fea751793de30006fcbf7ddb029a
Brahmasta Bagus Aryandra <brahmastabagus@gmail.com>
2024-01-01 12:45:14 +0700

    feat(contact_image): add controller and routes

---
+++ routes/contactImage.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const auth = require('../middleware/auth')
+const contactImageController = require('../controllers/contactImageController')
+const checkRole = require('../middleware/checkRole')
+
+router.get("/", contactImageController.getContactImage)
+router.delete("/:id", auth, checkRole(["admin"]), contactImageController.deleteContactImage)
+
+module.exports = router
